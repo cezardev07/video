73cca2043c5fc36b24692627961102388784eb38
cezardev07 <103595580+cezardev07@users.noreply.github.com>
2022-06-20 19:53:25 -0300

    add js

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector(".form");
+        const search = document.querySelector(".fm-inp");
+        const btn = document.querySelector(".btn-search");
+
+        btn.addEventListener("click", function(){
+            if (search.value.length > 0) {
+                window.location = "https://cezardev07.github.io/" + search.value;
+                search.value = "";
+            }else {
+                form.classList.toggle("actived");
+            }
+        })
