812a5ca8aa5fc818df67dccf60ddd3375de41930
cezardev07 <103595580+cezardev07@users.noreply.github.com>
2022-07-25 13:06:47 -0300

    add script search

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const form = document.querySelector(".form");
-        const search = document.querySelector(".fm-inp");
-        const btn = document.querySelector(".btn-search");
+// ======================================= add sistema de pesquisa =======================================
 
-        btn.addEventListener("click", function(){
-            if (search.value.length > 0) {
-                window.location = "https://cezardev07.github.io/" + search.value;
-                search.value = "";
-            }else {
-                form.classList.toggle("actived");
+const form = document.querySelector(".form");
+const search = document.querySelector(".fm-inp");
+const btn = document.querySelector(".btn-search");
+
+    btn.addEventListener("click", function(){
+        if (search.value.length > 0) {
+            window.location = "https://cezardev07.github.io/" + search.value;
+            search.value = "";
+        }else {
+            form.classList.toggle("actived");
             }
-        })
+        });
+        
+// ======================================= add sistema de tecla de pesquisa ===============================
+
+    document.addEventListener("keypress", function(e) {
+        if (e.key === "Enter"){
+            const btn = document.querySelector(".btn-search");
+            btn.click();
+            }
+        });
